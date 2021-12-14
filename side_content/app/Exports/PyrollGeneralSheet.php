67cae0d11eac8131cdd83bf74bf15375648fa5fd
alejandratese@gmail.com <Asulan_88>
2021-12-14 07:36:56 -0600

    Estilos de la hoja

--- side_content/app/Exports/PyrollGeneralSheet.php
+++ side_content/app/Exports/PyrollGeneralSheet.php
@@ -33,7 +33,7 @@ class PyrollGeneralSheet implements FromArray, WithTitle, ShouldAutoSize, WithSt
     {
         return [
             'A' => '@',
-            'B' => '@',
+            'B' => '"$"#,##0.00_-',
             'C' => '"$"#,##0.00_-',
             'D' => '"$"#,##0.00_-',
             'E' => '"$"#,##0.00_-',
@@ -55,8 +55,30 @@ class PyrollGeneralSheet implements FromArray, WithTitle, ShouldAutoSize, WithSt
         $sheet->mergeCells('A2:B2');
         $sheet->mergeCells('C2:E2');
         $sheet->mergeCells('G2:L2');
+
         for($i = 5; $i < 2 + 4; $i++ ){
             $sheet->getRowDimension($i)->setRowHeight(25);
         }
+
+        return [
+            'A1' => ['font' => ['bold' => true, 'size' => 14], 'alignment' => ['horizontal' => 'left']],
+            'A2' => ['font' => ['bold' => true, 'size' => 20], 'alignment' => ['horizontal' => 'center']],
+            'C2' => ['font' => ['bold' => true, 'size' => 20], 'alignment' => ['horizontal' => 'center']],
+            'G2' => ['font' => ['bold' => true, 'size' => 20], 'alignment' => ['horizontal' => 'center']],
+
+            'A4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'left']],
+            'B4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'C4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'D4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'E4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'F4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'G4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'H4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'I4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'J4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'K4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'L4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'center']],
+            'M4' => ['font' => ['bold' => true], 'alignment' => ['horizontal' => 'left']],
+        ];
     }
 }
